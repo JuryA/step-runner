6248db0dee31426c781a4e60551e22852247d4f4
Joseph Burnett <jburnett@gitlab.com>
2024-01-02 17:25:26 -0800

    Use protovalidate

--- pkg/step/validate.go
+++ pkg/step/validate.go
@@ -2,61 +2,18 @@ package step
 
 import (
 	"fmt"
-	"regexp"
 
+	"github.com/bufbuild/protovalidate-go"
 	"gitlab.com/gitlab-org/step-runner/proto"
 )
 
-// TODO replace this validation with a JSON schema for the steps model with syntactic sugar.
-
 func ValidateStepDefinition(stepDef *proto.StepDefinition) error {
-	errs := validateStepDefinitionIdentifiers(stepDef)
-	if len(errs) > 0 {
-		return fmt.Errorf("errors validating step definition: %v", errs)
+	v, err := protovalidate.New()
+	if err != nil {
+		return fmt.Errorf("failed to initialize validator: %w", err)
+	}
+	if err = v.Validate(stepDef); err != nil {
+		return fmt.Errorf("error validating step definition: %w", err)
 	}
 	return nil
-}
-
-func validateStepDefinitionIdentifiers(stepDef *proto.StepDefinition) []error {
-	errs := []error{}
-	validateIdentifier := func(k string) {
-		if !isAlphaNumeric(k) {
-			errs = append(errs, fmt.Errorf("identifer must be alphanumeric. got %q", k))
-		}
-	}
-	if stepDef.Spec != nil && stepDef.Spec.Spec != nil {
-		for k := range stepDef.Spec.Spec.Inputs {
-			validateIdentifier(k)
-		}
-		for k := range stepDef.Spec.Spec.Outputs {
-			validateIdentifier(k)
-		}
-	}
-	if stepDef.Definition != nil {
-		if stepDef.Definition != nil && stepDef.Definition.Steps != nil {
-			for _, s := range stepDef.Definition.Steps {
-				validateIdentifier(s.Name)
-				for k := range s.Env {
-					validateIdentifier(k)
-				}
-				for k := range s.Inputs {
-					validateIdentifier(k)
-				}
-			}
-			for k := range stepDef.Definition.Outputs {
-				validateIdentifier(k)
-			}
-		}
-	}
-	return errs
-}
-
-var alphaNumeric *regexp.Regexp
-
-func init() {
-	alphaNumeric = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
-}
-
-func isAlphaNumeric(k string) bool {
-	return alphaNumeric.MatchString(k)
 }
