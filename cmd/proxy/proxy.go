a400eb593f88b37fbf73e0a3bcb4542f95221db5
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-01-25 14:47:38 -0700

    Add proxy subcommand

---
+++ cmd/proxy/proxy.go
@@ -0,0 +1,62 @@
+package proxy
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// Note: proxying between stdin/out/err (the client) and gRPC (the server)
+
+var Cmd = &cobra.Command{
+	Use:   "proxy",
+	Short: "proxy commands from to step-runner server",
+	Args:  cobra.ExactArgs(0),
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	return Proxy(os.Stdout, os.Stdin, "tcp", "localhost:8765")
+}
+
+// Proxy connects the read and writer with the dialed connection.
+func Proxy(w io.Writer, r io.Reader, network, address string) error {
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return fmt.Errorf("proxy dialing: %w", err)
+	}
+
+	errChan := make(chan error)
+	// pipe stdin to the connection
+	go func() {
+		_, err := io.Copy(conn, r)
+		if err != nil {
+			err = fmt.Errorf("proxy r to conn: %w", err)
+		}
+		errChan <- err
+	}()
+
+	// pipe the connection to stdout
+	go func() {
+		_, err := io.Copy(w, conn)
+		if err != nil {
+			err = fmt.Errorf("proxy w to conn: %w", err)
+		}
+		errChan <- err
+	}()
+
+	err1 := <-errChan
+	err2 := conn.Close()
+	err3 := <-errChan
+
+	if err1 != nil {
+		return err1
+	}
+	if err2 != nil {
+		return err2
+	}
+	return err3
+}
--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 
 	"gitlab.com/gitlab-org/step-runner/cmd"
 	"gitlab.com/gitlab-org/step-runner/cmd/ci"
+	"gitlab.com/gitlab-org/step-runner/cmd/proxy"
 	"gitlab.com/gitlab-org/step-runner/cmd/service"
 )
 
 func main() {
 	cmd.RootCmd.AddCommand(ci.Cmd)
 	cmd.RootCmd.AddCommand(service.Cmd)
+	cmd.RootCmd.AddCommand(proxy.Cmd)
 	err := cmd.RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
