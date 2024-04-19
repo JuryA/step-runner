ca2a1334ac4d61d1712d5fda2ca31bed13a35b0a
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-04-19 16:09:05 -0600

    Implement Run API
    This is an initial implementation of the Run API. Most of the fun is in
    the Run function, which parses the step in the request, prepares the
    execution environment (job and environment variables, work and temp
    dirs), and finally initiates concurrent execution of the request.
    
    The run method actually runs the request, captures the output, and
    updates the associates Job.

---
+++ pkg/api/service/service.go
@@ -0,0 +1,98 @@
+// package service implements the gRPC API declared in ../../../proto/step.proto
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"gitlab.com/gitlab-org/step-runner/pkg/cache"
+	"gitlab.com/gitlab-org/step-runner/pkg/jobs"
+	"gitlab.com/gitlab-org/step-runner/pkg/runner"
+	"gitlab.com/gitlab-org/step-runner/pkg/step"
+	"gitlab.com/gitlab-org/step-runner/pkg/syncmap"
+	"gitlab.com/gitlab-org/step-runner/pkg/variables"
+	"gitlab.com/gitlab-org/step-runner/proto"
+)
+
+type StepRunnerService struct {
+	proto.StepRunnerServer
+	cache cache.Cache
+
+	jobs *syncmap.SyncMap[string, *jobs.Job]
+}
+
+func New() (*StepRunnerService, error) {
+	c, err := cache.New()
+	if err != nil {
+		return nil, fmt.Errorf("creating step-runner-service: %w", err)
+	}
+	return &StepRunnerService{
+		cache: c,
+		jobs:  syncmap.New[string, *jobs.Job](),
+	}, nil
+}
+
+// Run parses, prepares, and initiates execution of a RunRequest.
+func (s *StepRunnerService) Run(ctx context.Context, request *proto.RunRequest) (*proto.RunResponse, error) {
+	execution, err := runner.New(s.cache)
+	if err != nil {
+		return nil, fmt.Errorf("creating execution: %w", err)
+	}
+
+	steps, err := s.loadSteps(request.Steps)
+	if err != nil {
+		return nil, fmt.Errorf("loading step: %w", err)
+	}
+
+	job, err := jobs.New(request)
+	if err != nil {
+		return nil, fmt.Errorf("initializing request: %w", err)
+	}
+
+	jobVars, err := variables.Prepare(request.Job, job.TmpDir)
+	if err != nil {
+		return nil, fmt.Errorf("preparing environment: %w", err)
+	}
+
+	job.GlobCtx.Job = variables.Expand(jobVars)
+	if request.Env != nil {
+		job.GlobCtx.Env = request.Env
+	}
+
+	// last chance to bail...
+	if ctx.Err() != nil {
+		job.Close()
+		return nil, ctx.Err()
+	}
+
+	// actually execute the steps request
+	s.jobs.Put(request.Id, job)
+	go s.run(execution, job, steps)
+	return &proto.RunResponse{}, nil
+}
+
+func (s *StepRunnerService) loadSteps(stepsStr string) (*proto.StepDefinition, error) {
+	stepDef, err := step.ReadSteps(stepsStr, "")
+	if err != nil {
+		return nil, fmt.Errorf("reading steps %q: %w", stepsStr, err)
+	}
+
+	steps, err := step.CompileSteps(stepDef)
+	if err != nil {
+		return nil, fmt.Errorf("compiling steps: %w", err)
+	}
+
+	return steps, nil
+}
+
+// run actually starts execution of the steps request and captures the result. It is intended to be run in a goroutine.
+func (s *StepRunnerService) run(execution *runner.Execution, job *jobs.Job, steps *proto.StepDefinition) {
+	// TODO: Add streaming of step-results as they are produced.
+	result, err := execution.Run(job.Ctx, job.GlobCtx, &runner.Params{}, steps)
+	job.Finish(result, err)
+	if err != nil {
+		// TODO: better logging
+		log.Printf("an error occurred executing the job: %s", err)
+	}
+}
---
+++ pkg/api/service/service_test.go
@@ -0,0 +1,248 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/step-runner/pkg/jobs"
+	"gitlab.com/gitlab-org/step-runner/proto"
+)
+
+const (
+	id        = "853"
+	helloStep = `spec: {}
+---
+steps:
+  - name: hello_world
+    step: ../../runner/test_steps/greeting
+    inputs: {}
+`
+	bashStep = `spec: {}
+---
+steps:
+  - name: bash
+    step: ./testdata/bash
+    inputs:
+        script: %s
+`
+)
+
+func makeBashStep(cmd string) string {
+	return fmt.Sprintf(bashStep, cmd)
+}
+
+func testDirName(t *testing.T) string {
+	return path.Join(os.TempDir(), t.Name())
+}
+
+func makeRunRequest(t *testing.T, step string, withJob bool) *proto.RunRequest {
+	testDir := testDirName(t)
+	runReq := proto.RunRequest{
+		Id:    id,
+		Steps: step,
+		Env:   map[string]string{},
+	}
+
+	if withJob {
+		runReq.Job = &proto.Job{BuildDir: testDir}
+	} else {
+		runReq.WorkDir = testDir
+	}
+
+	return &runReq
+}
+
+func Test_StepRunnerService_Run_Success(t *testing.T) {
+	srs, err := New()
+	require.NoError(t, err)
+
+	rr := makeRunRequest(t, helloStep, false)
+
+	_, err = srs.Run(context.Background(), rr)
+	require.NoError(t, err)
+
+	job, ok := srs.jobs.Get(id)
+	require.True(t, ok)
+	defer os.RemoveAll(job.WorkDir)
+
+	assert.Eventually(t, job.Finished, time.Second*10, time.Millisecond*50)
+
+	assert.NoError(t, job.Ctx.Err())
+
+	res, err := job.Result()
+	assert.Nil(t, err)
+	require.NotNil(t, res)
+
+	assert.Equal(t, int32(0), res.ExitCode)
+
+	job.Close()
+	assert.NoDirExists(t, job.TmpDir)
+}
+
+func Test_StepRunnerService_Run_Cancelled(t *testing.T) {
+	defer os.RemoveAll(testDirName(t))
+
+	tests := map[string]struct {
+		id       string
+		script   string
+		finish   func(*jobs.Job, *StepRunnerService, *sync.WaitGroup)
+		validate func(*jobs.Job)
+	}{
+		"Close called before request executed": {
+			script: "sleep 1",
+			finish: func(j *jobs.Job, srs *StepRunnerService, wg *sync.WaitGroup) {
+				defer wg.Done()
+				j.Close()
+			},
+			validate: func(j *jobs.Job) {
+				res, err := j.Result()
+				assert.True(t, errors.Is(err, context.Canceled))
+				assert.Nil(t, res)
+			},
+		},
+		"Close called after request finished": {
+			script: "sleep 1",
+			finish: func(j *jobs.Job, srs *StepRunnerService, wg *sync.WaitGroup) {
+				defer wg.Done()
+				// Make sure the step execution finished
+				assert.Eventually(t, j.Finished, time.Millisecond*1900, time.Millisecond*100)
+				j.Close()
+			},
+			validate: func(j *jobs.Job) {
+				res, err := j.Result()
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+			},
+		},
+		"Close called before request finishes": {
+			script: "sleep 1",
+			finish: func(j *jobs.Job, srs *StepRunnerService, wg *sync.WaitGroup) {
+				defer wg.Done()
+				// Make sure the step sub-process was executed before calling Finish()
+				time.Sleep(time.Millisecond * 50)
+				j.Close()
+			},
+			validate: func(j *jobs.Job) {
+				res, err := j.Result()
+				assert.True(t, errors.Is(err, context.Canceled))
+				assert.Nil(t, res)
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			srs, err := New()
+			require.NoError(t, err)
+
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+
+			_, err = srs.Run(context.Background(), makeRunRequest(t, makeBashStep(tt.script), false))
+			require.NoError(t, err)
+
+			job, ok := srs.jobs.Get(id)
+			require.True(t, ok)
+			defer os.RemoveAll(job.WorkDir)
+
+			go tt.finish(job, srs, &wg)
+
+			assert.Eventually(t, job.Finished, time.Millisecond*5500, time.Millisecond*100)
+			wg.Wait()
+
+			assert.Error(t, job.Ctx.Err())
+			tt.validate(job)
+
+			assert.NoDirExists(t, job.TmpDir)
+		})
+	}
+}
+
+func Test_StepRunnerService_Run_Vars(t *testing.T) {
+	defer os.RemoveAll(testDirName(t))
+
+	tests := map[string]struct {
+		jobWorkDir bool
+		id         string
+		script     string
+		setup      func(*proto.RunRequest)
+	}{
+		"env vars": {
+			id:     "111",
+			script: "echo ${{ env.BAR}} > ${{ env.FOO }}",
+			setup: func(rr *proto.RunRequest) {
+				rr.Id = "111"
+				rr.Env = map[string]string{
+					"FOO": "blammo.txt",
+					"BAR": "foobarbaz",
+				}
+			},
+		},
+		"job vars": {
+			jobWorkDir: true,
+			id:         "222",
+			script:     "echo ${{ job.BAR}} > ${{ job.FOO }}",
+			setup: func(rr *proto.RunRequest) {
+				rr.Id = "222"
+				rr.Job.Variables = []*proto.Variable{
+					{Key: "FOO", Value: "blammo.txt"},
+					{Key: "BAR", Value: "foobarbaz"},
+				}
+			},
+		},
+		"job file vars": {
+			jobWorkDir: true,
+			id:         "333",
+			script:     "cat ${{ job.BAR}} > ${{ job.FOO }}",
+			setup: func(rr *proto.RunRequest) {
+				rr.Id = "333"
+				rr.Job.Variables = []*proto.Variable{
+					{Key: "FOO", Value: "blammo.txt"},
+					{Key: "BAR", Value: "foobarbaz", File: true},
+				}
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			srs, err := New()
+			require.NoError(t, err)
+
+			rr := makeRunRequest(t, makeBashStep(tt.script), tt.jobWorkDir)
+			tt.setup(rr)
+
+			_, err = srs.Run(context.Background(), rr)
+			require.NoError(t, err)
+
+			job, ok := srs.jobs.Get(tt.id)
+			require.True(t, ok)
+			defer os.RemoveAll(job.WorkDir)
+
+			assert.Eventually(t, job.Finished, time.Millisecond*500, time.Millisecond*50)
+			assert.NoError(t, job.Ctx.Err())
+
+			res, err := job.Result()
+			assert.Nil(t, err)
+			require.NotNil(t, res)
+
+			assert.Equal(t, int32(0), res.ExitCode)
+			assert.FileExists(t, path.Join(job.WorkDir, "blammo.txt"))
+			data, err := os.ReadFile(path.Join(job.WorkDir, "blammo.txt"))
+			require.NoError(t, err)
+			assert.Equal(t, "foobarbaz", strings.TrimSpace(string(data)))
+
+			job.Close()
+			assert.NoDirExists(t, job.TmpDir)
+		})
+	}
+}
