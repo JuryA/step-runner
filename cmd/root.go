3523cf271c3a2806c0932a075c6677aa0c7ae70b
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-04-29 16:36:14 -0600

    Don't print usage when command returns error

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import "github.com/spf13/cobra"
 
 var RootCmd = &cobra.Command{
-	Use:   "step-runner",
-	Short: "Step Runner executes a series of CI steps",
+	Use:          "step-runner",
+	Short:        "Step Runner executes a series of CI steps",
+	SilenceUsage: true,
 }
