c648eec821095e032fc101f73340dd482fd246cc
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-04-29 16:35:46 -0600

    Implement serve sub-command
    Initial and minimal implementation of the serve sub-command. Though TBH
    aside from some command line options, I'm not sure there will be much
    more to this command.

---
+++ cmd/serve/serve.go
@@ -0,0 +1,53 @@
+package serve
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"gitlab.com/gitlab-org/step-runner/pkg/api/service"
+	"google.golang.org/grpc"
+
+	"gitlab.com/gitlab-org/step-runner/proto"
+)
+
+const socketPath = "/tmp/step-runner.sock"
+
+var Cmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start StepRunner service",
+	Args:  cobra.ExactArgs(0),
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	var grpcServer *grpc.Server
+	sigChan := make(chan os.Signal, 1)
+
+	go func() {
+		signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+		sig := <-sigChan
+		log.Printf("received '%s' signal; shutting down.", sig.String())
+		grpcServer.GracefulStop()
+	}()
+
+	srv, err := service.New()
+	if err != nil {
+		return fmt.Errorf("failed to create step-runner request handler: %w", err)
+	}
+
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return fmt.Errorf("failed to open open socket %q for listening: %w", socketPath, err)
+	}
+
+	grpcServer = grpc.NewServer()
+	proto.RegisterStepRunnerServer(grpcServer, srv)
+
+	log.Printf("step-runner service listening on %v", listener.Addr())
+	return grpcServer.Serve(listener)
+}
