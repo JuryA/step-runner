64b906954b4909ca6fb0128746282e95889fe4fd
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-04-29 16:36:01 -0600

    Hook up server sub-command

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 
 	"gitlab.com/gitlab-org/step-runner/cmd"
 	"gitlab.com/gitlab-org/step-runner/cmd/ci"
+	"gitlab.com/gitlab-org/step-runner/cmd/serve"
 )
 
 func main() {
 	cmd.RootCmd.AddCommand(ci.Cmd)
+	cmd.RootCmd.AddCommand(serve.Cmd)
 	err := cmd.RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
