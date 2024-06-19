994aab4f49e345f3004873be1e6049939147df0d
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-06-19 11:28:49 -0600

    In-memory Streamer
    As implementation of the Streamer idea, that buffers data in a
    slice. We'll use this later to enable streaming of StepResults.
    
    The notable part here is the use of `sync.Cond` to synchronise the
    writer and reader(s). On the follower side, `Cond.Wait()` will stop the
    calling gorouteine(s) and thus pause when all StepResults collected so
    far have been forwarded. On the Writer side calling `Cond.Broadcast()`
    after a StepResult has been collected will wake up all stopped
    goroutines.
    
    The Follower(s) must be told when to stop via the Stop() method.

---
+++ pkg/internal/streamer/memory/memory.go
@@ -0,0 +1,55 @@
+// package memory implements a simple way to facilitate "live" steaming of data originating from a single source, to
+// multiple clients. The Write() method is used by the producer side to capture and accumulate (i.e. buffer) new data.
+// The Follow() method is used by the consumer side to subscribe to and receive data. Calling Follow() will write all
+// data buffered so far, as well as all future data the producer side writes. Follow() will block until Stop() is
+// called, which tells the consumer side to stop waiting for new data and return. Data is buffered in-memory. This type
+// should only be used when the data being buffered is bounded and small (enough).
+
+package memory
+
+import (
+	"sync"
+)
+
+type Streamer[T any] struct {
+	cond *sync.Cond
+	data []T
+	stop bool
+}
+
+func New[T any]() *Streamer[T] {
+	return &Streamer[T]{cond: sync.NewCond(&sync.Mutex{})}
+}
+
+func (s *Streamer[T]) Write(v T) {
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+	s.data = append(s.data, v)
+	s.cond.Broadcast()
+}
+
+func (s *Streamer[T]) Stop() {
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+	s.stop = true
+	s.cond.Broadcast()
+}
+
+func (s *Streamer[T]) Follow(write func(T) error) error {
+	i := 0
+	for {
+		s.cond.L.Lock()
+		for ; i < len(s.data); i++ {
+			if err := write(s.data[i]); err != nil {
+				s.cond.L.Unlock()
+				return err
+			}
+		}
+		if s.stop {
+			s.cond.L.Unlock()
+			return nil
+		}
+		s.cond.Wait()
+		s.cond.L.Unlock()
+	}
+}
---
+++ pkg/internal/streamer/memory/memory_test.go
@@ -0,0 +1,120 @@
+package memory
+
+import (
+	"errors"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/gitlab-org/step-runner/proto"
+)
+
+func makeStepResults(n int) []*proto.StepResult {
+	result := []*proto.StepResult{}
+	for i := 0; i < n; i++ {
+		result = append(result, &proto.StepResult{ExecResult: &proto.StepResult_ExecResult{ExitCode: int32(i)}})
+	}
+	return result
+}
+
+func Test_Streamer(t *testing.T) {
+	var gotResults []*proto.StepResult
+
+	numSourceResults := 5
+
+	tests := map[string]struct {
+		writer      func(*proto.StepResult) error
+		validate    func(error)
+		wantResults int
+	}{
+		"happy path": {
+			wantResults: numSourceResults,
+			writer: func(sr *proto.StepResult) error {
+				gotResults = append(gotResults, sr)
+				return nil
+			},
+			validate: func(e error) {
+				assert.NoError(t, e)
+			},
+		},
+		"writer returns error": {
+			wantResults: 3,
+			writer: func(sr *proto.StepResult) error {
+				gotResults = append(gotResults, sr)
+				if len(gotResults) < 3 {
+					return nil
+				}
+				return errors.New("POW!!!")
+			},
+			validate: func(e error) {
+				assert.ErrorContains(t, e, "POW!!!")
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			s := New[*proto.StepResult]()
+			gotResults = []*proto.StepResult{}
+			wg := sync.WaitGroup{}
+
+			wg.Add(1)
+			var err error
+			go func() {
+				defer wg.Done()
+				err = s.Follow(tt.writer)
+			}()
+
+			sourceResults := makeStepResults(numSourceResults)
+			for _, result := range sourceResults {
+				s.Write(result)
+			}
+
+			s.Stop()
+
+			assert.Eventually(t, func() bool {
+				return len(gotResults) == tt.wantResults
+			}, 100*time.Millisecond, 25*time.Millisecond)
+
+			wg.Wait()
+
+			tt.validate(err)
+			for i := range gotResults {
+				assert.Equal(t, sourceResults[i].ExecResult.ExitCode, gotResults[i].ExecResult.ExitCode)
+			}
+		})
+	}
+}
+
+func Test_Streamer_StopBeforeFollow(t *testing.T) {
+	s := New[*proto.StepResult]()
+	gotResults := []*proto.StepResult{}
+	wg := sync.WaitGroup{}
+
+	sourceResults := makeStepResults(5)
+	for _, result := range sourceResults {
+		s.Write(result)
+	}
+	s.Stop()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := s.Follow(func(sr *proto.StepResult) error {
+			gotResults = append(gotResults, sr)
+			return nil
+		})
+		assert.NoError(t, err)
+	}()
+
+	assert.Eventually(t, func() bool {
+		return len(gotResults) == len(sourceResults)
+	}, 100*time.Millisecond, 25*time.Millisecond)
+
+	wg.Wait()
+
+	for i := range gotResults {
+		assert.Equal(t, sourceResults[i].ExecResult.ExitCode, gotResults[i].ExecResult.ExitCode)
+	}
+}
