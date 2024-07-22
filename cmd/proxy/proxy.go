432f8636e6ae9b6422c1006d957910a0cdeca604
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-07-22 16:06:28 -0600

    Proxy command
    Creates connection to gRPC service, and invokes Proxy with stdin/stdout
    as the source/sink and the gRPC connection.

---
+++ cmd/proxy/proxy.go
@@ -0,0 +1,27 @@
+package proxy
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"gitlab.com/gitlab-org/step-runner/pkg/api"
+	proxyapi "gitlab.com/gitlab-org/step-runner/pkg/api/proxy"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "proxy",
+	Short: "Tunnel gRPC requests/responses from stdin/stdout to the service listening on a local socket",
+	Args:  cobra.ExactArgs(0),
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	conn, err := net.Dial("unix", api.DefaultSocketPath())
+	if err != nil {
+		return fmt.Errorf("dialing: %w", err)
+	}
+	return proxyapi.Proxy(os.Stdin, os.Stdout, conn)
+}
--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 
 	"gitlab.com/gitlab-org/step-runner/cmd"
 	"gitlab.com/gitlab-org/step-runner/cmd/ci"
+	"gitlab.com/gitlab-org/step-runner/cmd/proxy"
 	"gitlab.com/gitlab-org/step-runner/cmd/serve"
 )
 
 func main() {
 	cmd.RootCmd.AddCommand(ci.Cmd)
 	cmd.RootCmd.AddCommand(serve.Cmd)
+	cmd.RootCmd.AddCommand(proxy.Cmd)
 	err := cmd.RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
