5e61a3c278a0cee2863813fd8ee15673015f67c6
Axel von Bertoldi <avonbertoldi@gitlab.com>
2024-10-17 15:57:02 -0600

    Don't print out version when executing the proxy command
    Because the proxy reads from stdin and write to stdout on one side of
    the connection, printing anything to stdout breaks the whole thing. The
    relevant error message is.
    
        executing step request: running job request: rpc error: code = Unavailable desc = connection error: desc = "error reading server preface: http2: frame too large"
    
    To fix the above I had to move printing of the version to the root
    command, where I can detect what subcommand is being run. To to the
    latter I had to make some changes to how ldflags are handled.

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,24 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// stepRunnerVersion is set when the step runner is compiled in the Dockerfile
+var stepRunnerVersion = "UNKNOWN (unset in build flags)"
 
 func NewRootCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:          "step-runner",
 		Short:        "Step Runner executes a series of CI steps",
 		SilenceUsage: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if cmd.CalledAs() != "proxy" {
+				fmt.Printf("\nStep Runner version: %s\n", stepRunnerVersion)
+				fmt.Printf("See https://gitlab.com/gitlab-org/step-runner/-/blob/main/CHANGELOG.md for changes.\n\n")
+			}
+		},
 	}
 }
