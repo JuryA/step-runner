edff44d6cece3a92ec6ae56c2fe1b7d517c1c7ee
Joseph Burnett <jburnett@gitlab.com>
2024-10-18 16:34:17 -0700

    Create basic and extended clients

--- pkg/internal/delegate/grpc_delegate.go
+++ pkg/internal/delegate/grpc_delegate.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"os"
 
+	"gitlab.com/gitlab-org/step-runner/pkg/api/client/basic"
+	"gitlab.com/gitlab-org/step-runner/pkg/api/client/extended"
 	"gitlab.com/gitlab-org/step-runner/proto"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type GRPCOutputer struct {
-	id         string
-	socketFile string
+	jobId          string
+	basicClient    *basic.StepRunnerClient
+	extendedClient *extended.StepRunnerClient
+}
+
+type alreadyDialed struct {
+	conn *grpc.ClientConn
+}
+
+func (a *alreadyDialed) Dial() (*grpc.ClientConn, error) {
+	return a.conn, nil
 }
 
 func LoadFromFile(filename string) (*GRPCOutputer, error) {
@@ -23,12 +35,23 @@ func LoadFromFile(filename string) (*GRPCOutputer, error) {
 	if err := protojson.Unmarshal(data, grpcDelegate); err != nil {
 		return nil, fmt.Errorf("reading output_file as grpc delegate: %w", err)
 	}
+	conn, err := grpc.Dial(grpcDelegate.SocketFile, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	basicClient := basic.New(conn)
+	extendedClient, err := extended.New(&alreadyDialed{conn})
+	if err != nil {
+		return nil, err
+	}
 	return &GRPCOutputer{
-		id:         grpcDelegate.Id,
-		socketFile: grpcDelegate.SocketFile,
+		jobId:          grpcDelegate.Id,
+		basicClient:    basicClient,
+		extendedClient: extendedClient,
 	}, nil
 }
 
 func (o *GRPCOutputer) Outputs() (map[string]*structpb.Value, *proto.StepResult, error) {
+
 	return nil, nil, nil
 }
