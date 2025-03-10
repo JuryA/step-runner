b86d4376ceac9c9ebe1ccabc779db15f0d699ede
Cameron Swords <cswords@gitlab.com>
2025-03-10 13:58:32 +0000

    Update compile to use built in step

---
+++ e2e_tests/script_test.go
@@ -0,0 +1,22 @@
+package e2e_tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"gitlab.com/gitlab-org/step-runner/pkg/testutil"
+)
+
+func TestCanRunAScript(t *testing.T) {
+	stepYml := `
+spec:
+---
+run:
+  - name: my_script
+    script: echo hi`
+
+	_, logs, err := testutil.StepRunner(t).Run(stepYml)
+	require.NoError(t, err)
+	require.Contains(t, logs, "hi")
+}
