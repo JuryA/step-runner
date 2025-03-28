f009ded0ff567fdf95ac809067eeefad6711c680
Cameron Swords <cswords@gitlab.com>
2025-03-28 12:05:55 +0000

    Move exec type and methods to exec file

--- schema/v1/step.go
+++ schema/v1/step.go
@@ -7,35 +7,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type Exec struct {
-	// Command are the parameters to the system exec API. It does not invoke a shell.
-	Command []string `json:"command" yaml:"command" mapstructure:"command"`
-
-	// WorkDir is the working directly in which `command` will be exec'ed.
-	WorkDir *string `json:"work_dir,omitempty" yaml:"work_dir,omitempty" mapstructure:"work_dir,omitempty"`
-}
-
-// UnmarshalJSON implements json.Unmarshaler.
-func (j *Exec) UnmarshalJSON(b []byte) error {
-	var raw map[string]interface{}
-	if err := json.Unmarshal(b, &raw); err != nil {
-		return err
-	}
-	if _, ok := raw["command"]; raw != nil && !ok {
-		return fmt.Errorf("field command in Exec: required")
-	}
-	type Plain Exec
-	var plain Plain
-	if err := json.Unmarshal(b, &plain); err != nil {
-		return err
-	}
-	if plain.Command != nil && len(plain.Command) < 1 {
-		return fmt.Errorf("field %s length: must be >= %d", "command", 1)
-	}
-	*j = Exec(plain)
-	return nil
-}
-
 // Step is a unit of execution.
 type Step struct {
 	// Action is a GitHub action to run.
