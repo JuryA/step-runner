47b523949e1591b308bc678c0776fd2cda208d88
Cameron Swords <cswords@gitlab.com>
2025-04-17 13:06:29 +0100

    Expressions can be used when loading steps from file

--- pkg/runner/step_resource_parser.go
+++ pkg/runner/step_resource_parser.go
@@ -3,7 +3,6 @@ package runner
 import (
 	"fmt"
 	"path/filepath"
-	"strings"
 
 	"gitlab.com/gitlab-org/step-runner/pkg/cache/dist"
 	"gitlab.com/gitlab-org/step-runner/pkg/cache/git"
@@ -22,30 +21,37 @@ func NewStepResourceParser(gitFetcher *git.GitFetcher, distFetcher *dist.Fetcher
 	}
 }
 
-func (p *StepResourceParser) Parse(parentDir string, stepRef *proto.Step_Reference) (StepResource, error) {
-	stepDir := filepath.Join(stepRef.Path...)
+func (p *StepResourceParser) Parse(workDir string, stepRef *proto.Step_Reference) (StepResource, error) {
+	stepPath := p.stepPath(stepRef)
 
 	switch stepRef.Protocol {
 	case proto.StepReferenceProtocol_local:
-		dir := stepDir
-		if !strings.HasPrefix(stepDir, "/") {
-			dir = filepath.Join(parentDir, stepDir)
-		}
-
-		return NewFileSystemStepResource(dir, stepRef.Filename), nil
+		return NewFileSystemStepResource(workDir, stepPath, stepRef.Filename), nil
 
 	case proto.StepReferenceProtocol_git:
-		return NewGitStepResource(p.gitFetcher, stepRef.Url, stepRef.Version, stepDir, stepRef.Filename), nil
+		return NewGitStepResource(p.gitFetcher, stepRef.Url, stepRef.Version, stepPath, stepRef.Filename), nil
 
 	case proto.StepReferenceProtocol_dist:
-		return NewDistStepResource(p.distFetcher, stepDir, stepRef.Filename), nil
+		return NewDistStepResource(p.distFetcher, stepPath, stepRef.Filename), nil
 
 	case proto.StepReferenceProtocol_dynamic:
 		return NewDynamicStepResource(p, stepRef.Url), nil
 
 	case proto.StepReferenceProtocol_spec_def:
-		return NewFixedStepResource(NewSpecDefinition(stepRef.SpecDef.Spec, stepRef.SpecDef.Definition, stepDir)), nil
+		return NewFixedStepResource(NewSpecDefinition(stepRef.SpecDef.Spec, stepRef.SpecDef.Definition, stepPath)), nil
 	}
 
 	return nil, fmt.Errorf("unknown step reference protocol: %s", stepRef.Protocol)
+}
+
+func (p *StepResourceParser) stepPath(stepRef *proto.Step_Reference) string {
+	if val, ok := stepRef.StepPath.(*proto.Step_Reference_PathExp); ok {
+		return val.PathExp
+	}
+
+	if val, ok := stepRef.StepPath.(*proto.Step_Reference_Paths); ok {
+		return filepath.Join(val.Paths.Parts...)
+	}
+
+	return filepath.Join(stepRef.Path...)
 }
