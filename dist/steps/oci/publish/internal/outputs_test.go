c6d81ca18e8213d4b88e9ec4caa995db6a822c99
Cameron Swords <cswords@gitlab.com>
2025-04-24 10:55:13 +0100

    Rename oci publish module so go get can find it

--- dist/steps/oci/publish/internal/outputs_test.go
+++ dist/steps/oci/publish/internal/outputs_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/stretchr/testify/require"
 
-	"gitlab.com/gitlab-org/dist-steps/oci/publish/internal"
+	"gitlab.com/gitlab-org/step-runner/dist/steps/oci/publish/internal"
 
 	mainBldr "gitlab.com/gitlab-org/step-runner/pkg/testutil/bldr"
 )
